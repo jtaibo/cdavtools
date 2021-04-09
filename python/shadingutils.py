632b66f3a59fad4c194ba69797aaf77a67019fbb
Javier Taibo <javier.taibo@gmail.com>
2021-04-09 17:21:35 +0200

    Added new shelf button: Reassign materials to all instances of selected objects

--- python/shadingutils.py
+++ python/shadingutils.py
@@ -1,11 +1,16 @@
 import maya.cmds as cmd
 
 '''
+
 Functions defined in this module:
 
-    getInstancedNodeFromSelection()
     checkPerFaceMaterialsInAnyInstance(shape)
     getPerFaceMaterialAssignment(transf, shape)
+    reassignPerFaceMaterialsToInstances(transform, shape, all_shape_instances)
+
+Entry points (called from Shelf buttons):
+
+    reassignMaterials()
 
 '''
 
@@ -15,15 +20,22 @@ Functions defined in this module:
 #   Get transform and shape from a single object selection considering
 #   instanced nodes (shape is common, transforms are different)
 #
-#   Returns a list with two elements: transform and shape (in this order)
+#   Returns a list with three elements: transform and shape and 
+#   instances_array (in this order)
 #
+#   NOTE: This function is not currently used. Just ignore it.
+#   Nothing to see here, move along...
+#
+###############################################################################
 def getInstancedNodeFromSelection():
     # Get all possible paths for the (potentially) instanced node
     shape_paths = cmd.ls(selection=True, dag=True, leaf=True, allPaths=True, long=True)
     if not shape_paths:
         return
-    #print("shape_paths: ", shape_paths)
+    #print("Shape_paths: ", shape_paths)
     transform = cmd.ls(selection=True)
+    #print("Transforms: ", transform)
+
     if len(transform) == 1:
         transform = transform[0]
     else:
@@ -41,7 +53,7 @@ def getInstancedNodeFromSelection():
     #WARNING: when selecting nodes higher in hierarchy, the transform may not be the direct parent of the shape (may be a higher group)
     # TO-DO: fix!
 
-    return [transform, shape]
+    return [transform, shape, shape_paths]
 
 
 ###############################################################################
@@ -51,6 +63,7 @@ def getInstancedNodeFromSelection():
 #
 #   Returns True if there are several SEs, otherwise False
 #
+###############################################################################
 def checkPerFaceMaterialsInAnyInstance(shape):
     if not shape:
         cmd.warning("No shape selected")
@@ -65,6 +78,7 @@ def checkPerFaceMaterialsInAnyInstance(shape):
 #
 #    Returns a map of faces with shadingEngine as key
 #
+###############################################################################
 def getPerFaceMaterialAssignment(transf, shape):
     if not shape:
         cmd.warning("No shape selected")
@@ -78,11 +92,49 @@ def getPerFaceMaterialAssignment(transf, shape):
         for sg in sgs:
             faces = []
             members = cmd.sets(sg, q=True)
-            #print(members)
-            for item in members:
-                if transf in item:
-                     faces.append(item)
+            if members:
+                for item in members:
+                    if transf in item:
+                         faces.append(item)
             if faces:
                 mat2face[sg] = faces
     return mat2face
 
+
+###############################################################################
+#
+#    Reassign per-face materials to every other instance of the shape
+#
+###############################################################################
+def reassignPerFaceMaterialsToInstances(transform, shape, all_shape_instances):
+    mat2face = getPerFaceMaterialAssignment(transform, shape)
+    if not mat2face:
+        # No material, assign default one
+        print("No material. Assigning default SG")
+        cmd.sets(transform, e=True, forceElement="initialShadingGroup")
+        mat2face = getPerFaceMaterialAssignment(transform, shape)
+    # For each instane of the shape...
+    for s in all_shape_instances:
+        if not transform in s:  # ...except the original (selected) one, of course
+            if len(mat2face) == 1:
+                # Per object material assignment (or some faces with no material)
+                sg = mat2face.keys()[0]
+                cmd.sets(s, e=True, forceElement=sg)
+            else:
+                for sg in mat2face:
+                    for f in mat2face[sg]:
+                        new_faces = f.replace(transform, s)
+                        cmd.sets(new_faces, e=True, forceElement=sg)
+
+
+###############################################################################
+#
+#    Reassign per-face materials to every other instance of the shape
+#
+###############################################################################
+def reassignMaterials():
+    transforms = cmd.ls(selection=True)
+    for t in transforms:
+        shape = cmd.listRelatives(t, shapes=True, fullPath=True)
+        all_shape_instances = cmd.ls(shape, allPaths=True)
+        reassignPerFaceMaterialsToInstances(t, shape, all_shape_instances)
