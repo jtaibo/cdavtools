c7a9471b685c8317af0fc4c896d5697199565963
Javier Taibo <javier.taibo@gmail.com>
2024-04-05 20:20:14 +0200

    Updated paths to 2024 version

--- render/env.bat
+++ render/env.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set ADSKFLEX_LICENSE_FILE=@licsuad
-set MAYA_BIN="C:\Program Files\Autodesk\Maya2022\bin"
-set ARNOLD_BIN="C:\Program Files\Autodesk\Arnold\maya2022\bin"
+set MAYA_BIN="C:\Program Files\Autodesk\Maya2024\bin"
+set ARNOLD_BIN="C:\Program Files\Autodesk\Arnold\maya2024\bin"
 set MRVIEWER_BIN="C:\Program Files\mrViewer-v5.6.0-Windows-64\bin"
 
 REM Uncomment the line below when using a custom OCIO config file (not the default one in Maya 2022)
